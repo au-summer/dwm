4ca048eb1c9cdd04d8b44663ddfb606153b4916a
ausummer <ausummer@umich.edu>
2025-01-13 06:59:18 -0500

    feat: init

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources 
+xbacklight -set 10 &
+feh --bg-fill ~/Pictures/Background/apple.png &
+xset r rate 200 50 &
+picom &
+lxpolkit &
+ibus-daemon &
+autorandr --change
+
+dash ~/.config/chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
